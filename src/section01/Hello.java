1ca5f03adfadce18c4876f91d33957fdebb47056
SonSETO <yesthis9090@gmail.com>
2023-06-05 20:54:07 +0900

    주석 추가

--- src/section01/Hello.java
+++ src/section01/Hello.java
@@ -1,7 +1,23 @@
 package section01;
+/* 
+ * 
+ * 
+ * 여러 줄 주석 
+ * 
+ * 
+ * 
+ * 
+  */
 
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, Java");
+
+		printNum(516);
 	}
+
+	public static void printNum(int num) {
+		System.out.println("num: " + num);
+	}
+
 }
